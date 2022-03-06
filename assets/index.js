67ba90daf039818294715221ab8d673fcd55b030
Daniil <traitnil@gmail.com>
2022-03-07 00:36:07 +0200

    RenderingTwoElements

--- assets/index.js
+++ assets/index.js
@@ -5,10 +5,32 @@
 
 
 
-const reactElement = React.createElement("h1",{ 
-    title: "hello", 
-    className: "heading" 
-},
-  "Hello, React.js!");
+class Heading extends React.Component{
 
-ReactDOM.render(reactElement, document.getElementById("root"));
+  render(){
+    console.log(this.props);
+    const {titleForHeading, classForHeading, children} = this.props;
+    
+    return React.createElement('h1', {
+      title: titleForHeading,
+      className: classForHeading
+    },
+      ...children);
+  }
+}
+
+const reactElement = React.createElement(Heading, {
+  titleForHeading: 'hi',
+  classForHeading: 'heading'
+},'atr1');
+
+const reactElement2 = React.createElement(Heading, {
+
+  classForHeading: 'new-heading'
+}, 'New Heading');
+
+const reactWrapperElement = React.createElement('div', null, reactElement, reactElement2);
+
+
+
+ReactDOM.render(reactWrapperElement, document.getElementById("root"));
